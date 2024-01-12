e7f4e92a357a29713835d9445fa8a212fbff7ed1
HeckerCreatives <34593098+Ganielle@users.noreply.github.com>
2024-01-12 08:47:22 +0800

    Update Auth.js

--- controllers/Auth.js
+++ controllers/Auth.js
@@ -9,7 +9,7 @@ exports.login = (req, res) => {
             return res.status(400).json({ message: "bad-request", data: "User not found! Please input your true credentials" })
         }
 
-        res.json({message: "success"})
+        res.json({message: "success", data: data})
     })
     .catch(error => res.status(400).json({ message: "bad-request", data: error.message }))
 }
